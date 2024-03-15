5c7ca19c6f3f7f26a374072975fe64d104e0c925
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-03-15 17:02:14 -0300

    Ajuste

--- CalculoImposto/Servico/INSS/Interface/IINSSServico.cs
+++ CalculoImposto/Servico/INSS/Interface/IINSSServico.cs
@@ -1,24 +1,23 @@
 ﻿using CalculoImposto.Modelo.DTO.INSS;
 
-namespace CalculoImposto.API.Servico.INSS.Interface
+namespace CalculoImposto.API.Servico.INSS.Interface;
+
+public interface IINSSServico
 {
-    public interface IINSSServico
-    {
-        Task<IEnumerable<INSSDto>> PegarTodosInss(int pagina, int tamanho, string busca);
-        Task<IEnumerable<INSSDto>> PegarTodosPorCompetenciaInss(DateTime competencia);
+    Task<IEnumerable<INSSDto>> PegarTodosInss(int pagina, int tamanho, string busca);
+    Task<IEnumerable<INSSDto>> PegarTodosPorCompetenciaInss(DateTime competencia);
 
-        Task<int> PegarFaixaInss(DateTime competencia, decimal baseInss);
-        Task<int> UltimaFaixaCompetenciaInss(DateTime competencia);
+    Task<int> PegarFaixaInss(DateTime competencia, decimal baseInss);
+    Task<int> UltimaFaixaCompetenciaInss(DateTime competencia);
 
-        Task<decimal> PorcentagemFaixaCompetenciaInss(DateTime competencia, int faixa);
-        Task<decimal> ValorFaixaCompetenciaInss(DateTime competencia, int faixa);
-        Task<decimal> ValorTetoCompetenciaInss(DateTime competencia);
+    Task<decimal> PorcentagemFaixaCompetenciaInss(DateTime competencia, int faixa);
+    Task<decimal> ValorFaixaCompetenciaInss(DateTime competencia, int faixa);
+    Task<decimal> ValorTetoCompetenciaInss(DateTime competencia);
 
-        Task<INSSDto> PegarPorIdInss(int id);
-        Task CriarInss(INSSDto inss);
-        Task AtualizarInss(INSSDto inss);
-        Task DeletarInss(int id);
+    Task<INSSDto> PegarPorIdInss(int id);
+    Task CriarInss(INSSDto inss);
+    Task AtualizarInss(INSSDto inss);
+    Task DeletarInss(int id);
 
-        Task<int> TotalInss(string busca);
-    }
+    Task<int> TotalInss(string busca);
 }
