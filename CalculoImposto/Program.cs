1fcc9263d1a781c3cab2115935cec869abbba7fa
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-03-19 13:54:24 -0300

    Ajuste

--- CalculoImposto/Program.cs
+++ CalculoImposto/Program.cs
@@ -38,13 +38,8 @@ builder.Services.AddScoped<ICalculoImpostoServico, CalculoImpostoServico>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculo Imposto v1"));
 app.UseAuthorization();
 
 app.MapControllers();
