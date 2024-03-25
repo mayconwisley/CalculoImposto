81b47e4877499a12d9c77e7a917e9eec2f8543b4
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-03-25 10:28:40 -0300

    Add Testes

--- CalculoImposto.Teste/Calculos/CalculosTeste.cs
+++ CalculoImposto.Teste/Calculos/CalculosTeste.cs
@@ -1,6 +1,7 @@
 ﻿using CalculoImposto.API.Controllers;
 using CalculoImposto.API.Servico.Calculo.Interface;
 using CalculoImposto.Modelo.DTO.INSS;
+using CalculoImposto.Modelo.DTO.IRRF;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -16,7 +17,6 @@ public class CalculosTeste
     {
         _mockCalculoImposto = new Mock<ICalculoImpostoServico>();
         _calculosController = new CalculosController(_mockCalculoImposto.Object);
-
     }
 
     [TestMethod]
@@ -26,11 +26,59 @@ public class CalculosTeste
         var baseInss = 5000m;
 
         _mockCalculoImposto.Setup(x => x.CalculoInssProgressivo(It.IsAny<DateTime>(), It.IsAny<decimal>()))
-            .ReturnsAsync(new CalculoInssProgressivoDto());
+                                        .ReturnsAsync(new CalculoInssProgressivoDto());
 
-        var result = await _calculosController.Calculo(strCompetencia, baseInss);
-
+        var result = await _calculosController.CalculoInssProgressivo(strCompetencia, baseInss);
         Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+    }
 
+    [TestMethod]
+    public async Task CalculoIrrfProgressivo()
+    {
+        var strCompetencia = "01/01/2024";
+        var valorIrrf = 5000m;
+        var baseInss = 5000m;
+        var qtDependente = 0;
+
+        _mockCalculoImposto.Setup(x => x.CalculoIrrfProgressivo(It.IsAny<DateTime>(),
+                                                                It.IsAny<decimal>(),
+                                                                It.IsAny<decimal>(),
+                                                                It.IsAny<int>()))
+                                        .ReturnsAsync(new CalculoProgressivoDto());
+
+        var result = await _calculosController.CalculoIrrfProgressivo(strCompetencia, valorIrrf, baseInss, qtDependente);
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+    }
+    [TestMethod]
+    public async Task CalculoIrrfNormal()
+    {
+        var strCompetencia = "01/01/2024";
+        var valorIrrf = 5000m;
+        var baseInss = 5000m;
+        var qtDependente = 0;
+
+        _mockCalculoImposto.Setup(x => x.CalculoIrrfNormal(It.IsAny<DateTime>(),
+                                                                It.IsAny<decimal>(),
+                                                                It.IsAny<decimal>(),
+                                                                It.IsAny<int>()))
+                                        .ReturnsAsync(new CalculoNormalDto());
+
+        var result = await _calculosController.CalculoIrrfNormal(strCompetencia, valorIrrf, baseInss, qtDependente);
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+    }
+
+    [TestMethod]
+    public async Task CalculoIrrfSimplificado()
+    {
+        var strCompetencia = "01/01/2024";
+        var valorIrrf = 5000m;
+
+        _mockCalculoImposto.Setup(x => x.CalculoIrrfSimplificado(It.IsAny<DateTime>(),
+                                                                It.IsAny<decimal>()
+                                                                ))
+                                        .ReturnsAsync(new CalculoSimplificadoDto());
+
+        var result = await _calculosController.CalculoIrrfSimplificado(strCompetencia, valorIrrf);
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
     }
 }
--- CalculoImposto/Controllers/CalculosController.cs
+++ CalculoImposto/Controllers/CalculosController.cs
@@ -12,7 +12,7 @@ public class CalculosController(ICalculoImpostoServico calculoImposto) : Control
     private readonly ICalculoImpostoServico _calculoImposto = calculoImposto;
 
     [HttpGet("Inss/Progressivo/{strCompetencia}/{baseInss:decimal}")]
-    public async Task<ActionResult<CalculoInssProgressivoDto>> Calculo(string strCompetencia, decimal baseInss)
+    public async Task<ActionResult<CalculoInssProgressivoDto>> CalculoInssProgressivo(string strCompetencia, decimal baseInss)
     {
         DateTime competencia = DateTime.Parse(strCompetencia.Replace("%2F", "/"));
         var calculoInss = await _calculoImposto.CalculoInssProgressivo(competencia, baseInss);
@@ -25,7 +25,7 @@ public class CalculosController(ICalculoImpostoServico calculoImposto) : Control
         return Ok(calculoInss);
     }
     [HttpGet("Irrf/Normal/{strCompetencia}/{valorBrutoIrrf:decimal}/{baseInss:decimal}/{qtDependente:int}")]
-    public async Task<ActionResult<CalculoNormalDto>> CalculoNormal(string strCompetencia, decimal valorBrutoIrrf, decimal baseInss, int qtDependente)
+    public async Task<ActionResult<CalculoNormalDto>> CalculoIrrfNormal(string strCompetencia, decimal valorBrutoIrrf, decimal baseInss, int qtDependente)
     {
         DateTime competencia = DateTime.Parse(strCompetencia.Replace("%2F", "/"));
         var calculoIrrf = await _calculoImposto.CalculoIrrfNormal(competencia, valorBrutoIrrf, baseInss, qtDependente);
@@ -38,7 +38,7 @@ public class CalculosController(ICalculoImpostoServico calculoImposto) : Control
         return Ok(calculoIrrf);
     }
     [HttpGet("Irrf/Progressivo/{strCompetencia}/{valorBrutoIrrf:decimal}/{baseInss:decimal}/{qtDependente:int}")]
-    public async Task<ActionResult<CalculoProgressivoDto>> CalculoProgressivo(string strCompetencia, decimal valorBrutoIrrf, decimal baseInss, int qtDependente)
+    public async Task<ActionResult<CalculoProgressivoDto>> CalculoIrrfProgressivo(string strCompetencia, decimal valorBrutoIrrf, decimal baseInss, int qtDependente)
     {
         DateTime competencia = DateTime.Parse(strCompetencia.Replace("%2F", "/"));
         var calculoIrrf = await _calculoImposto.CalculoIrrfProgressivo(competencia, valorBrutoIrrf, baseInss, qtDependente);
@@ -51,7 +51,7 @@ public class CalculosController(ICalculoImpostoServico calculoImposto) : Control
         return Ok(calculoIrrf);
     }
     [HttpGet("Irrf/Simplificado/{strCompetencia}/{valorBrutoIrrf:decimal}")]
-    public async Task<ActionResult<CalculoSimplificadoDto>> CalculoSimplificado(string strCompetencia, decimal valorBrutoIrrf)
+    public async Task<ActionResult<CalculoSimplificadoDto>> CalculoIrrfSimplificado(string strCompetencia, decimal valorBrutoIrrf)
     {
         DateTime competencia = DateTime.Parse(strCompetencia.Replace("%2F", "/"));
         var calculoIrrf = await _calculoImposto.CalculoIrrfSimplificado(competencia, valorBrutoIrrf);
