26fcf8e955570e793e2046eb91e4c9459062c6fa
Maycon Wisley Batista <maycon.wisley@outlook.com>
2025-03-20 10:06:00 -0300

    Remove Update use case and implement INSS update command
    - Removed folder reference for `UseCases\Inss\Update` in the project file.
    - Added `Command` record in `Command.cs` to handle INSS updates using MediatR.
    - Introduced `Handler` class in `Handler.cs` to process the update command and return results.
    - Created `Response` record in `Response.cs` to encapsulate the updated INSS entity.

---
+++ CalculoImposto.Application/UseCases/Inss/Update/Command.cs
@@ -0,0 +1,6 @@
+﻿using CalculoImposto.Domain.Abstractions;
+using MediatR;
+
+namespace CalculoImposto.Application.UseCases.Inss.Update;
+
+public sealed record Command(Domain.Entities.Inss Inss) : IRequest<Result<Response>>;
---
+++ CalculoImposto.Application/UseCases/Inss/Update/Response.cs
@@ -0,0 +1,3 @@
+﻿namespace CalculoImposto.Application.UseCases.Inss.Update;
+
+public sealed record Response(Domain.Entities.Inss Inss);
