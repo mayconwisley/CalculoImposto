1cf1f78eddcdd392ff449ae1f0a58fdc2145bf93
Maycon Wisley Batista <maycon.wisley@outlook.com>
2025-03-20 17:27:04 -0300

    Refactor InssController constructor and remove logic
    Updated InssController to use a parameterized constructor
    with ISender directly in the class definition. Removed
    command creation and response handling logic from
    GetAllAsync method, which may impact its functionality.

--- CalculoImposto/Controllers/InssController.cs
+++ CalculoImposto/Controllers/InssController.cs
@@ -10,10 +10,8 @@ public class InssController(ISender _sender) : ControllerBase
     [HttpGet]
     public async Task<ActionResult> GetAllAsync([FromQuery] int page = 1, [FromQuery] int size = 25, CancellationToken cancellationToken = default)
     {
-
         var command = new Application.UseCases.Inss.GetAll.Command(page, size);
         var result = await _sender.Send(command, cancellationToken);
-
 
         return result.IsSucess ? Ok(result.Value) :
                                  BadRequest(result.Error);
